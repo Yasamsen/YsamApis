b9b2faceace1b078afef0dc4a882b8d98b032199
ALyaSam <162271637+Yasamsen@users.noreply.github.com>
2026-09-03 08:57:14 +0800

    Delete api/index.js

--- api/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function handler(req, res) {
-  if (req.method === 'OPTIONS') return res.status(204).end();
-  return res.status(200).json({
-    success: true,
-    message: 'SamApi is online',
-    docs: '/docs.html'
-  });
-};
