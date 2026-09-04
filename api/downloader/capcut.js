a5419882f9f041a3657c65b2fc23e39f5b4cb66a
ALyaSam <162271637+Yasamsen@users.noreply.github.com>
2026-09-04 21:31:26 +0800

    Update api/downloader/capcut.js

--- api/downloader/capcut.js
+++ api/downloader/capcut.js
@@ -458,7 +458,7 @@ async function scrapeCapcut(inputUrl) {
  */
 
 module.exports = createApi({
-  name: "CapCut",
+  name: "CapCut Scraper",
 
   description:
     "Scrape metadata dan video dari URL template CapCut.",
