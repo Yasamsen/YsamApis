944b316b8d86bc472f7153fbc9373103a339d753
ALyaSam <162271637+Yasamsen@users.noreply.github.com>
2026-09-06 16:45:38 +0800

    Delete api/auth/logout.js

--- api/auth/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearSessionCookie } = require("../_lib/session");
-
-module.exports = async function handler(req, res) {
-  clearSessionCookie(res);
-  return res.json({ success: true, message: "Logged out." });
-};
